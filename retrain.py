7c7e44c2f37f31dfd796b37ff4b994f52197ab4e
LeCorreP <122294580+LeCorreP@users.noreply.github.com>
2023-03-30 15:18:32 +0200

    Delete retrain.py

--- retrain.py
+++
@@ -1,9 +0,0 @@
-# For retrain an existing autoencodeur 
-
-import Autoencodeur
-
-sexe,filenames=Autoencodeur.load_attr()
-
-for i in range(1,100):
-    Autoencodeur.retrain(sexe,filenames,1000,i*1000)
-    print(i)
