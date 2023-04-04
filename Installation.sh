670e78477656e9ba26bea63fb2ccc1a5b1a531ba
OririPla <128084892+OririPla@users.noreply.github.com>
2023-04-04 14:26:04 +0200

    Add files via upload

--- Installation.sh
+++ Installation.sh
@@ -8,4 +8,9 @@ pip install --upgrade wheel
 
 pip install Code-groupe1-laguilhon
 
+mv f_annexes env1/lib/python3.9/site-packages/src
+mv IMG env1/lib/python3.9/site-packages/src
+
+python3 env1/lib/python3.9/site-packages/src/Code/start_software_faces.py
+
 
---
+++ Start_log_2.sh
@@ -0,0 +1,2 @@
+python3 env1/lib/python3.9/site-packages/src/Code/start_software_faces.py
+
